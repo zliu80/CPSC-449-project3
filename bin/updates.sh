f7e003362a079402014474228bdd5b594cfd8775
ZyklonR <92772530+ZyklonR@users.noreply.github.com>
2022-12-17 17:38:27 -0800

    Add files via upload

---
+++ bin/updates.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Starting script"
+# Echoes the word "hello" in a file "hello.txt" in the same folder
+echo /usr/bin/wget --spider "http://127.0.0.0.1/leadboard" >> updates.txt
+echo "Finished"
