3ace75d7c913986dae11acde3efef5bf0de3498d
ZyklonR <92772530+ZyklonR@users.noreply.github.com>
2022-12-17 17:39:54 -0800

    Update updates.sh

--- bin/updates.sh
+++ bin/updates.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Starting script"
-# Echoes the word "hello" in a file "hello.txt" in the same folder
 echo /usr/bin/wget --spider "http://127.0.0.0.1/leadboard" >> updates.txt
 echo "Finished"
